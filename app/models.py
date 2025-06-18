ddd524736c002a9922330b18f1df6da3034a4ad8
Vignesh <vigneshganapathi1006@gmail.com>
2025-06-18 16:02:11 +0530

    [MOD]add field

--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field,String,Column,Integer
 from typing import Optional
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    mobile_number : int = Field(sa_column=Column("mobile_number",Integer,nullable=True))
     email: str = Field(index=True, unique=True)
     hashed_password: str
+
+class RoleMaster(SQLModel, table=True):
+    id : Optional[int]  = Field(default=None,primary_key=True)
+    name : str = Field(sa_column=Column("name",String,nullable=False,unique=True))
+    description : str = Field(sa_column=Column("description",String,nullable=True))
